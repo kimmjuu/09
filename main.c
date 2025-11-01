54ede06d6f05bb9323cbdc9a5261841bf2bb4476
kimmjuu <2014069@sookmyung.ac.kr>
2025-11-01 20:54:31 +0900

    09-2(1)

--- main.c
+++ main.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define SIZE 5
 
 int main(void)
 {
     int i;
-    int grade[5];
+    int grade[SIZE];
 
-    grade[0] = 10;
-    grade[1] = 20;
-    grade[2] = 30;
-    grade[3] = 40;
-    grade[4] = 50;
-    for (i = 0; i < 5; i++)
-    {
+    printf("Please enter 5 scores.\n");
+
+    for (i = 0; i < SIZE; i++)
+        scanf("%d", &grade[i]);
+
+    for (i = 0; i < SIZE; i++)
         printf("grade[%d] = %d\n", i, grade[i]);
-    }
 
     return 0;
 }
